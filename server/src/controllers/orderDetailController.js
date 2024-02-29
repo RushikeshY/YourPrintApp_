75787dcc78a6562d15a28c4526c377d51d952dfa
Rushikeshy007 <99939510+RushikeshY@users.noreply.github.com>
2024-02-29 14:36:59 +0530

    update orderdetail api

--- server/src/controllers/orderDetailController.js
+++ server/src/controllers/orderDetailController.js
@@ -1,12 +1,9 @@
 // const Order = require('../models/orderDetailModel');
 // const catchAsyncErrors = require('../middleware/catchAsyncErrors');
 
-
-
 // const nodemailer = require('nodemailer');
 
 // // Assuming you have the necessary configurations for your email service
-
 
 // exports.createOrder = catchAsyncErrors(async (req, res,next) => {
 //   const { products, shippingAddress, totalPrice, orderStatus } = req.body;
@@ -30,7 +27,6 @@
 //   },
 // });
 
-      
 //     const mailOptions = {
 //         from: "your_email@gmail.com", // Sender email address
 //         to: email,
@@ -91,14 +87,14 @@
 //                   Thank you for placing order. Your order will delivered soon.
 //                 </div>
 //                 <div class="otp">
-                          
+
 //                 </div>
 //               </div>
 //             </body>
 //           </html>
 //     `,
 //       };
-  
+
 //       await transporter.sendMail(mailOptions);
 //     res.status(201).json({
 //       success: true,
@@ -240,91 +236,160 @@
 //   `;
 // };
 
-const Order = require('../models/orderDetailModel');
-const catchAsyncErrors = require('../middleware/catchAsyncErrors');
-const nodemailer = require('nodemailer');
-const User = require('../models/User');
-const Product = require('../models/product'); // Replace 'Product' with your actual product model
+const Order = require("../models/orderDetailModel");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const nodemailer = require("nodemailer");
+const User = require("../models/User");
+const Product = require("../models/product"); // Replace 'Product' with your actual product model
 
 exports.createOrder = catchAsyncErrors(async (req, res, next) => {
   const { products, shippingAddress, totalPrice, orderStatus } = req.body;
   const userId = req.user.id;
-
+  console.log(products);
   try {
     const user = await User.findById(userId);
     const userEmail = user.email;
 
-    const productDetails = await getProductDetails(products);
+    // const productDetails = await getProductDetails(products);
+    // let productsDetails = [];
 
-    const order = await Order.create({
+    // for (let i = 0; i < products.length; i++) {
+    //   let id = products[i].id;
+    //   console.log(id);
+
+    //   let product1 = await Product.findById(id);
+    //   console.log(product1);
+
+    //   productsDetails.push(product1);
+
+    // }
+
+    // console.log(productDetails)
+
+    // const order = await Order.create({
+    //   user: userId,
+    //   products,
+    //   shippingAddress,
+    //   totalPrice,
+    //   orderStatus,
+    // });
+
+    // const populatedProducts = await Promise.all(
+    //   products.map(async (product) => {
+    //     const productDetails = await Product.findById(product.product);
+    //     return {
+    //       ...productDetails.toObject(),
+    //       quantity: product.quantity,
+    //     };
+    //   })
+    // );
+    // console.log(populatedProducts)
+
+    //Assuming product IDs are stored in the 'id' property of each orderedProduct
+    const productIds = products.map((orderedProduct) => orderedProduct.id);
+
+    // Fetch detailed product information using populate
+    const populatedProducts = await Product.find({ _id: { $in: productIds } });
+
+    // Combine product details with ordered quantity
+    const orderedProductsDetails = populatedProducts.map((product) => {
+      const orderedProduct = products.find(
+        (p) => p.id === product._id.toString()
+      ); // Use toString() to compare ObjectId
+      return {
+        ...product.toObject(),
+        quantity: orderedProduct.quantity,
+      };
+    });
+
+    console.log(orderedProductsDetails);
+
+    const newOrder = new Order({
       user: userId,
-      products: productDetails,
+      products,
       shippingAddress,
       totalPrice,
       orderStatus,
     });
 
+    await newOrder.save();
     const transporter = nodemailer.createTransport({
-      service: 'Gmail',
+      service: "Gmail",
       auth: {
         user: process.env.EMAIL, // Access email from environment variable
         pass: process.env.PASSWORD, // Access password from environment variable
       },
     });
 
-
     const mailOptions = {
-      from: 'your_email@gmail.com',
+      from: "your_email@gmail.com",
       to: userEmail,
-      subject: 'Invoice Of Placed Order',
-      html: generateInvoiceHtml(order),
+      subject: "Invoice Of Placed Order",
+      html: generateInvoiceHtml(orderedProductsDetails, newOrder),
     };
 
     await transporter.sendMail(mailOptions);
 
+    // Generate HTML invoice with populated products
+    // const invoiceHtml =
+
     res.status(201).json({
       success: true,
-      order,
-      message: 'Order placed successfully',
+      newOrder,
+      message: "Order placed successfully",
     });
   } catch (error) {
-    console.error('Error placing order:', error);
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
+    console.error("Error placing order:", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 });
 
-const getProductDetails = async (products) => {
-  const productDetails = [];
-  for (const product of products) {
-    const productDetail = await Product.findById(product.productId);
+// const getProductDetails = async (products) => {
+//   const productDetails = [];
+//   for (const product of products) {
+//     const productDetail = await Product.findById(product.productId);
 
-    console.log(productDetail)
+//     console.log(productDetail)
 
-    if (productDetail) {
-      productDetails.push({
-        productId: productDetail._id,
-        name: productDetail.productTitle,
-        price: productDetail.mrp,
-        quantity: product.quantity,
-        imageUrl: productDetail.imageUrl,
-      });
-    }
-  }
-  return productDetails;
-};
+//     if (productDetail) {
+//       productDetails.push({
+//         productId: productDetail._id,
+//         name: productDetail.productTitle,
+//         price: productDetail.mrp,
+//         quantity: product.quantity,
+//         imageUrl: productDetail.imageUrl,
+//       });
+//     }
+//   }
+//   return productDetails;
+// };
 
-const generateInvoiceHtml = (order) => {
-  const productsHtml = order.products.map((product) => `
+const generateInvoiceHtml = (orderedProductsDetails, order) => {
+  const productsHtml = orderedProductsDetails
+    .map(
+      (product) => `
     <tr>
       <td style="padding: 10px; border: 1px solid #ddd; text-align: center;">
-        <img src="${product.imageUrl}" alt="${product.productTitle}" style="max-width: 100%; height: auto;">
+        <img style="width:200px" src="https://img.freepik.com/free-photo/wooden-shelf-tropical-forest-product-presentation-dark-green-background_41470-5139.jpg?w=900&t=st=1709188542~exp=1709189142~hmac=ce75e0364b70858fad4cf7722eb1a9ca9965c6c3b0e854a06693bcb2affa6844" alt="${
+          product.productTitle
+        }" style="max-width: 100%; height: auto;">
       </td>
-      <td style="padding: 10px; border: 1px solid #ddd; text-align: center;">${product.productTitle}</td>
-      <td style="padding: 10px; border: 1px solid #ddd; text-align: center;">${product.mrp}</td>
-      <td style="padding: 10px; border: 1px solid #ddd; text-align: center;">${product.quantity}</td>
-      <td style="padding: 10px; border: 1px solid #ddd; text-align: center;">${product.mrp * product.quantity}</td>
+      <td style="padding: 10px; border: 1px solid #ddd;font-weight:bold;  text-align: center;"> ${
+        product.productTitle
+      }</td>
+      <td style="padding: 10px; border: 1px solid #ddd;font-weight:bold; text-align: center;">₹${
+        product.mrp
+      }</td>
+      <td style="padding: 10px; border: 1px solid #ddd; font-weight:bold; text-align: center;">${
+        product.quantity
+      }</td>
+      <td style="padding: 10px; border: 1px solid #ddd; font-weight:bold;  text-align: center;"₹${
+        product.mrp * product.quantity
+      }</td>
     </tr>
-  `).join('');
+  `
+    )
+    .join("");
 
   return `
     <html>
@@ -366,10 +431,9 @@ const generateInvoiceHtml = (order) => {
       </head>
       <body>
         <div class="container">
-          <div class="logo">
-            <img src="https://your-logo-url.com" alt="Your Logo" style="max-width: 100%;">
-          </div>
-          <h2>Invoice for Order #${order._id}</h2>
+        <h3 style="text-align:center">Thank you for placing an order. Your order will be delivered soon.</h3>
+
+          <h2 style="text-align:center">Invoice for Order #${order._id}</h2>
           <table class="invoice-table">
             <thead>
               <tr>
@@ -385,7 +449,6 @@ const generateInvoiceHtml = (order) => {
             </tbody>
           </table>
           <p>Total Price: ${order.totalPrice}</p>
-          <p>Thank you for placing an order. Your order will be delivered soon.</p>
         </div>
       </body>
     </html>
